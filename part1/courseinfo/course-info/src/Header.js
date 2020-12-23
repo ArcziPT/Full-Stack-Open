f9baaa7305be2e9627eeab8bec2ec38dc29deb61
ArcziPT <awolk@student.agh.edu.pl>
2020-12-23 19:25:31 +0100

    exercise 1.1

---
+++ part1/courseinfo/course-info/src/Header.js
@@ -0,0 +1,5 @@
+export const Header = (props) => (
+    <div>
+        <h1>{props.course}</h1>
+    </div>
+)
---
+++ part1/courseinfo/course-info/src/Total.js
@@ -0,0 +1,5 @@
+export const Total = (props) => (
+    <div>
+        <p>Number of exercises {props.exercises}</p>
+    </div>
+)
