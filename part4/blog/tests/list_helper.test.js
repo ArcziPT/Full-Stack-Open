2f2a300eaf9b74ecddd9c44fcf66acb1e0aaeb68
ArcziPT <awolk@student.agh.edu.pl>
2021-01-12 12:22:33 +0100

    exercise 4.6

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -50,3 +50,44 @@ describe('favourite blog', () => {
         expect(result).toEqual(blogs[1])
     })
 })
+
+describe('most blogs', () => {
+    const blogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'John',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        ,{
+          _id: '5a422aa7af54a676234d1700',
+          title: 'JS for dummies',
+          author: 'John',
+          url: 'http://jsfordummies.js',
+          likes: 1100,
+          __v: 0
+        },
+        {
+          _id: '5a422aa71b54a676234d17f9',
+          title: 'New Blog',
+          author: 'Cormen',
+          url: 'http://algorithms.com',
+          likes: 999,
+          __v: 0
+        },{
+          _id: '5a422aa71b54a676234d1700',
+          title: 'JS',
+          author: 'JS Master',
+          url: 'http://js.js',
+          likes: 100,
+          __v: 0
+        }
+    ]
+
+    test('from list of blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        expect(result).toEqual({author: 'John', blogs: 2})
+    })
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,4 +2,24 @@ const totalLikes = (blogs) => blogs.map(b => b.likes).reduce((acc, v) => acc + v
 
 const favouriteBlog = (blogs) => blogs.reduce((fBlog, cBlog) => fBlog.likes > cBlog.likes ? fBlog : cBlog)
 
-module.exports = {totalLikes, favouriteBlog}
+const mostBlogs = (blogs) => {
+    const authors = {}
+    const a = blogs.reduce((author, blog) => {
+        if(blog.author in authors){
+            authors[blog.author] += 1
+            if(authors[blog.author] > authors[author])
+                return blog.author
+            return author
+        }
+        else{
+            authors[blog.author] = 1
+            return author
+        }
+    }, blogs[0].author)
+
+    console.log(a)
+
+    return {author: a, blogs: authors[a]}
+}
+
+module.exports = {totalLikes, favouriteBlog, mostBlogs}
