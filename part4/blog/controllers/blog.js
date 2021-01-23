03b13f9539a3a72f69ce991805046d7efb433ebf
ArcziPT <awolk@student.agh.edu.pl>
2021-01-23 22:16:41 +0100

    exercise 4.14

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -30,4 +30,13 @@ blogRouter.delete('/:id', async (request, response) => {
     response.status(200).send(result)
 })
 
+blogRouter.put('/:id', async (request, response) => {
+    const result = await Blog.findByIdAndUpdate(request.params.id, request.body)
+    
+    if(!result)
+        response.status(404).end()
+    
+    response.status(200).send(result)
+})
+
 module.exports = blogRouter
