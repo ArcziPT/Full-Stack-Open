98b078ecaec34e20b63fe60a6b3ca0edf258044e
ArcziPT <awolk@student.agh.edu.pl>
2021-02-05 11:48:30 +0100

    exercise 5.18

--- part5/bloglist-frontend/cypress/integration/blog.test.js
+++ part5/bloglist-frontend/cypress/integration/blog.test.js
@@ -1,11 +1,31 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        // create here a user to backend
+        cy.request('POST', 'http://localhost:3000/api/users', {username: "user", password:"pass"})
         cy.visit('http://localhost:3000')
     })
   
     it('Login form is shown', function() {
         cy.contains('login').click()
         cy.get('.loginForm')
+    })
+  
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.contains('login').click()
+            cy.get('#username').type('user')
+            cy.get('#password').type('pass')
+            cy.get('.loginForm').contains('login').click()
+            cy.contains('user is logged in')
+        })
+  
+        it('fails with wrong credentials', function() {
+            cy.contains('login').click()
+            cy.get('#username').type('user123')
+            cy.get('#password').type('pass123')
+            cy.get('.loginForm').contains('login').click()
+            cy.contains('user is logged in').should('not.exist')
+        })
     })
 })
